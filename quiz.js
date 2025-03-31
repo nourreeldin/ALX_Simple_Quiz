63cabf381071599f7580e76bd6740e26d4a99cd0
Noureldin <noureeldinhossamm@gmail.com>
2025-04-01 01:01:23 +0200

    Make some changes

--- quiz.js
+++ quiz.js
@@ -3,7 +3,7 @@ function checkAnswer() {
     let selectedAnswer = document.querySelector('[name="quiz"]:checked');
     if (selectedAnswer) {
         let userAnswer = selectedAnswer.getAttribute('value');
-        if (userAnswer == correctAnswer)
+        if (userAnswer === correctAnswer)
             document.getElementById('feedback').textContent = "Correct! Well done.";
         else
             document.getElementById('feedback').textContent = "That's incorrect. Try again!";
